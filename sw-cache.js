ee08bb512cfd2bffd6cec53075058c9e75a07d52
Victoria <bernard.victoria22@gmail.com>
2018-05-12 23:33:03 -0500

    Caching working

--- sw-cache.js
+++ sw-cache.js
@@ -1,87 +1,35 @@
-const CACHE = 'meetup-cache';
-URLArr = ['./plugin/zoom-js/zoom.js']
-addURLsToCache = (URLs) =>{
-    console.log('addurls')
-    return caches.open(CACHE)
-    .then((cache) =>{
-        console.log('adda alls')
-        return cache.addAll(URLs)
-    })
-    .catch((err) =>{
-        console.log('There was an error pre caching')
-        return Promise.reject('issue')
-    })
-}
 var CACHE_FILES = [
-    // '/sw.html',
-    '/',
-    // 'http://localhost:8080/src/images/*kitty.jpg',
-    'http://localhost:8080/src/images/xmaskitty.jpg',
-    '/lib/css/zenburn.css',
-    // '/lib/js/head.min.js',
-    // '/lib/JOHN.jpg'
+    // '/whatever-i-want-to-precache.jpg',
+    // '/more-precache.html',
+    'http://localhost:8080/src/images/xmaskitty.jpg'
 ];
-var CACHE_VERSION = 'app-v1';
-var someFunc = function (){
-    console.log("waiting huntil")
+var EXCLUDED_FILES = [
+    '/src/images/x'
+]
+var CACHE_VERSION = 'name-of-my-cache';
+var preCacheFunc = function(){
     return caches.open(CACHE_VERSION)
         .then(function (cache) {
-            console.log('Opened cache');
             return cache.addAll(CACHE_FILES);
         })
 }
-
-self.addEventListener('activate', function(event){
-    // event.waitUntil(addURLsToCache(URLArr))
-    console.log('install service workers')
-
-    event.waitUntil(
-        (() => {
-            console.log("waiting huntil")
-            return caches.open(CACHE_VERSION)
-                .then(function (cache) {
-                    console.log('Opened cache');
-                    return cache.addAll(CACHE_FILES);
-                })
-        })()
-     )
-    })
-self.addEventListener('activate', function(event){
-    console.log('waited')
-    event.waitUntil(addURLsToCache(URLArr))
-    console.log('waited2')
-
+self.addEventListener('install', function (event) {
+    event.waitUntil(preCacheFunc()) 
+})
+self.addEventListener('fetch', function (event) {
+    event.respondWith(grabFromCacheOrAdd(event.request))
 });
-self.addEventListener('fetch', function(event){
-    URLArr.push(event.request.url)
-    // return something for each interception
-    console.log('fetching')
-    let whatIgotFromCache = grabFromCache(event.request)
-    console.log('from cache', whatIgotFromCache)
-    event.respondWith(whatIgotFromCache)
-    // event.respondWith(new Response('whwate i want'))
-});
-function grabFromCache(req) {
+function grabFromCacheOrAdd(req) {
     return caches.open(CACHE_VERSION).then(function (cache) {
-        // console.log(cache)
         return cache.match(req).then(function (matching) {
-            console.log(matching)
-            if (matching){
-                console.log('match', req.url)
+            if (matching) {
+                console.log('matched!')
                 return matching
-            }else{
-                console.log('no match',req.url)
-                // ret
+            } else {
+                console.log('missed!')
+                // if(CACHEreq.url)
                 return cache.add(req)
-                // response = fetch(req)
-                // response.then((resp) =>{
-                //     cache.add(req)
-
-                // })
-                // return response
-                // return matching || Promise.reject('no-match');
-
             }
-         });
+        });
     });
 }
