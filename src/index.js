5866fb39eae7ac00beb7dae7e4344b810071b1da
Victoria <bernard.victoria22@gmail.com>
2018-05-14 18:00:32 -0500

    code transition and format really, reallly  working

--- src/index.js
+++ src/index.js
@@ -1,18 +1,17 @@
 // include index.html for webpack to load
-// require("reveal.js/lib/js/head.min.js")
-// require("./index.html");
+require("reveal.js/lib/js/head.min.js")
+// require("./src/index.html");
 const Reveal = require('reveal.js')
 require('reveal.js/css/reveal.css')
 // require('reveal.js/css/theme/night.css')
 require('reveal.js/css/theme/solarized.css')
-// require('reveal.js/lib/css/zenburn.css')
+require('reveal.js/lib/css/zenburn.css')
 
 Reveal.initialize({
     controls: true,
     progress: true,
     history: true,
     center: true,
-    fragments: true,
     
     transition: 'slide', // none/fade/slide/convex/concave/zoom
     
@@ -26,17 +25,7 @@ Reveal.initialize({
         { src: 'plugin/notes/notes.js', async: true }
     ]
 });
-Reveal.addEventListener('fragmentshown', function (event) {
-    // console.log("fragment shown")
-    console.log("fragment shown", event)
-    console.log(event.fragment)
-});
-Reveal.addEventListener('fragmenthidden', function (event) {
-    // console.log("fragment hidden")
-    console.log("fragment hidden", event)
-    console.log(event.fragment)
-});
 
-// window.Reveal = Reveal;
+window.Reveal = Reveal;
 // console.log('foo')
 
