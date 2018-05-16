ecc84cbee33bf2f06c1483b6e21f9e85c444664a
Victoria <bernard.victoria22@gmail.com>
2018-05-15 20:25:50 -0500

    offline clean up

--- sw-offline.js
+++ sw-offline.js
@@ -14,57 +14,26 @@ self.addEventListener('install', function (event) {
     event.waitUntil(preCacheFunc()) 
 })
 self.addEventListener('fetch', function (event) {
-    // return event.respondWith(fetch(SAFETY_TMP))
-    let g =  fetch(event.request)
+    return event.respondWith(fetch(event.request)
         .then(resp => {
             if (!resp.ok) {
-                console.log('serving safe')
-                t = getSomeSafeResp(SAFETY_TMP)
-                console.log(t)
-                return t
+                return grabFromCacheOrAdd(SAFETY_TMP)
             }
             return resp
         })
         .catch(error => {
-            console.log(error)
-            console.log("Serving safe page")
-             return getSomeSafeResp(SAFETY_TMP)
+            return grabFromCacheOrAdd(SAFETY_TMP)
         })
-    console.log('resp', g)
-    event.respondWith(g)
-    // let somesafe = getSomeSafeResp(event.request)
-    // console.log('somesage', somesafe)
-    // event.respondWith(somesafe)
-});
-function getSomeSafeResp(req) {
+    )}
+);
+function grabFromCacheOrAdd(req) {
     return caches.open(CACHE_VERSION).then(function (cache) {
-        
-        return cache.match(req)//.then(function (matching) {
-        // })
-    })
+        return cache.match(req).then(function (matching) {
+            if (matching) {
+                return matching
+            } else {
+                return cache.add(req)
+            }
+        });
+    });
 }
-        // return cache.match(req).then(function (matching) {
-        //     if (matching) {
-        //         console.log('matched!')
-        //         return matching
-        //     } else {
-        //         console.log('missed!')
-        //         return fetch(req.url)
-                // .then( (response) =>{
-                //     if(!response.ok) throw new Error('asdasd')
-                //     console.log('success in populating the cache with tth response:', response)
-
-                //     cache.add(req)
-                    
-                //     return response
-                // })
-                // .catch(() => {
-                //     console.log('askjdaksjd errro!!')
-                //     t= cache.match(SAFETY_TMP)
-                //     console.log('templ', t)
-                //     return t
-                // })
-//             }
-//         });
-//     });
-// }
